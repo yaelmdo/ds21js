658c8accc5d45405389b88906c3f95d8bc5042b3
yaelmdo <68218415+yaelmdo@users.noreply.github.com>
2021-10-07 02:18:35 -0500

    Update selectionsort.js

--- 1p/4sorting/selectionsort.js
+++ 1p/4sorting/selectionsort.js
@@ -1,12 +1,13 @@
 //Maldonado Mendoza Yael
-      var arr=[1,9,2,3,7,6,4,5,7,8,56,34,33,12,5];
+      var pasos=0;
+    var arr=[1,9,2,3,7,6,4,5,7,8,56,34,33,12,5];
       console.log("array desordenado");
       console.log(arr);
      
     let n = arr.length;
         
     for(let i = 0; i < n; i++) {
-      
+       var e = new Date();
         let min = i;
         for(let j = i+1; j < n; j++){
             if(arr[j] < arr[min]) {
@@ -18,9 +19,10 @@
              let tmp = arr[i]; 
              arr[i] = arr[min];
              arr[min] = tmp;      
-        }
-    }
- 
-      console.log("array ordenado");
-      console.log(arr);
-    
+        }pasos++;
+    }    
+ var t = new Date() - e;
+console.log("array ordenado");
+console.log(arr);
+console.log('pasos:'+pasos);
+console.log('tiempo: '+t);
