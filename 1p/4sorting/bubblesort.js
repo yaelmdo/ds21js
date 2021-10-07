06e46ee26662201ca287454ad1a7ed31639e94bc
yaelmdo <68218415+yaelmdo@users.noreply.github.com>
2021-10-07 02:16:17 -0500

    Update bubblesort.js

--- 1p/4sorting/bubblesort.js
+++ 1p/4sorting/bubblesort.js
@@ -3,8 +3,11 @@ var arr=[1,9,2,3,7,6,4,5,5];
 console.log("array desordenado");
 console.log(arr);
 var len = arr.length;
-
+var pasos=0;
 for (var i = 0; i < len ; i++) {
+  var e = new Date();
+    
+    
   for(var j = 0 ; j < len - i - 1; j++){ // this was missing
   if (arr[j] > arr[j + 1]) {
     // swap
@@ -13,6 +16,10 @@ for (var i = 0; i < len ; i++) {
     arr[j + 1] = temp;
   }
  }
-}
+ pasos++;
+
+}var t = new Date() - e;
 console.log("array ordenado");
 console.log(arr);
+console.log('pasos:'+pasos);
+console.log('tiempo: '+t);
